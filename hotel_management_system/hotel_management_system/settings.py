65ce048f62c6d3ad0fa32c9e5029a8158f21fc9d
jordyloor2003 <jloorm2003@gmail.com>
2025-03-08 20:32:05 -0500

    Modify INSTALLED_APPS

--- hotel_management_system/hotel_management_system/settings.py
+++ hotel_management_system/hotel_management_system/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.Booking',
-    'apps.Hotel',
-    'apps.User',
+    'Booking',
+    'Hotel',
+    'User',
 ]
 
 MIDDLEWARE = [
