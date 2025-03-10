2f28882319bfe91d17b2c472a784fbe4af857583
jordyloor2003 <jloorm2003@gmail.com>
2025-03-09 20:31:52 -0500

    Create urls.py for User

--- hotel_management_system/User/urls.py
+++ hotel_management_system/User/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import views
+from .views import (UserDetailView, UserListView, UserRegisterView, UserUpdateView, CustomLoginView)
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-
+    path('login/', CustomLoginView.as_view(template_name="users/login.html"), name="login"),
+    path('logout/', LogoutView.as_view(template_name="users/logout.html"), name= "logout"),
+    path('', UserListView.as_view(), name= "user_list"),
+    path('profile/', UserDetailView.as_view(), name= "user_detail"),
+    path('edit-profile/', UserUpdateView.as_view(), name= "user_edit"),
+    path('register/', UserRegisterView.as_view(), name= "user_register"),
 ]
