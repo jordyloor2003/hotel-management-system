37cbee8bdeb8321cf109a9853026a07c466a738e
jordyloor2003 <jloorm2003@gmail.com>
2025-04-02 17:59:27 -0500

    modify structure for the admin panel

--- hotel_management_system/User/admin.py
+++ hotel_management_system/User/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+def deactivate_users(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+deactivate_users.short_description = "Deactivate selected users"
+
+def activate_users(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+activate_users.short_description = "Reactivate selected users"
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'phone_number', 'is_hotel_owner', 'is_customer', 'is_superuser')
+    search_fields = ('username', 'email', 'phone_number')
+    list_filter = ('is_hotel_owner', 'is_customer', 'is_superuser', 'is_active')
+    actions = [deactivate_users, activate_users] 
+
+admin.site.register(User, UserAdmin)
